52b12b0e0cbf61560727bfa828112c6459b1c0cb
Arnaud gonin <arnaud.gonin@live.fr>
2021-11-23 13:05:32 +0100

    feature(): add basic style and landing page

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+const port = 8080;
+
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+app.use('/assests', express.static('src/public'));
+
+app.get('/', (req, res) => {
+	res.render('pages/index.ejs');
+});
+
+app.listen(port, () => console.log(`server listening on localhost:${port}`));
