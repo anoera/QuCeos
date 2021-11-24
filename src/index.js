b1d2a31df6d83747ac3983481830259e4dc09052
Arnaud gonin <arnaud.gonin@live.fr>
2021-11-24 18:30:25 +0100

    feature(): changle color

--- src/index.js
+++ src/index.js
@@ -3,13 +3,31 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+const data = {
+	projectName: 'QuCeos',
+	sections: [
+		{
+			topic: 'Fondamentaux',
+			subtopics: ['Qubits', 'Superposition / intrication', 'Théorème du non-clonage', 'Téléportation quantique'],
+		},
+		{
+			topic: 'Algorithmie',
+			subtopics: [''],
+		},
+		{
+			topic: 'Cybersécurité',
+			subtopics: ['Algorithme de Shor', 'Quantum Key Distribution'],
+		},
+	],
+};
+
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
 app.use('/assets', express.static('src/public/'));
 
 app.get('/', (req, res) => {
-	res.render('pages/index.ejs');
+	res.render('pages/index.ejs', data);
 });
 
 app.get('/index2', (req, res) => {
