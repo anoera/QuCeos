67167919953e442214530b849c1a002fc141c11a
Arnaud gonin <arnaud.gonin@live.fr>
2021-11-25 16:19:09 +0100

    feature(): content improvement

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const data = {
 	sections: [
 		{
 			topic: 'Fondamentaux',
-			subtopics: ['Qubits', 'Superposition / intrication', 'Théorème du non-clonage'],
+			subtopics: ['Qubits', 'Notations de Dirac', 'Théorème du non-clonage'],
 		},
 		{
 			topic: 'Algorithmie',
